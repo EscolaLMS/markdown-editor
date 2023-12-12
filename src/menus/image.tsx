10e720a52111f24fe51166729237cdf137135707
Szymon <szymon.mr@gmail.com>
2023-12-12 10:13:46 +0100

    feat: add text and heading align options

--- src/menus/image.tsx
+++ src/menus/image.tsx
@@ -37,7 +37,7 @@ export default function imageMenuItems(
       tooltip: dictionary.alignCenter,
       icon: AlignImageCenterIcon,
       visible: true,
-      active: state =>
+      active: (state) =>
         isNodeActive(schema.nodes.image)(state) && isCenterAligned(state),
     },
     {
---
+++ src/nodes/Paragraph.tsx
@@ -0,0 +1,91 @@
+import { setBlockType } from "prosemirror-commands";
+import Node from "./Node";
+
+export default class Paragraph extends Node {
+  get name() {
+    return "paragraph";
+  }
+
+  get schema() {
+    return {
+      attrs: {
+        layoutClass: { default: null },
+      },
+      content: "inline*",
+      group: "block",
+      parseDOM: [
+        {
+          tag: "p",
+          getAttrs: (dom) => this.extractLayoutClassFromDOM(dom),
+        },
+      ],
+      toDOM: (node) => this.createDOMAttributes(node),
+    };
+  }
+
+  keys({ type }) {
+    return {
+      "Shift-Ctrl-0": setBlockType(type),
+    };
+  }
+
+  commands({ type }) {
+    const alignText = (state, dispatch, layoutClass) => {
+      const attrs = { layoutClass };
+      const { selection } = state;
+      const pos = selection.$from.pos - 1;
+      const node = state.doc.nodeAt(pos);
+      if (node) {
+        const newAttrs = { ...node.attrs, ...attrs };
+        const tr = state.tr.setNodeMarkup(pos, null, newAttrs);
+        dispatch(tr);
+        return true;
+      }
+      return false;
+    };
+
+    return {
+      setBlockType: () => setBlockType(type),
+      alignTextRight: () => (state, dispatch) =>
+        alignText(state, dispatch, "right"),
+      alignTextLeft: () => (state, dispatch) =>
+        alignText(state, dispatch, "left"),
+      alignTextCenter: () => (state, dispatch) =>
+        alignText(state, dispatch, "center"),
+    };
+  }
+
+  toMarkdown(state, node) {
+    if (this.isEmptyParagraph(node, state)) {
+      state.write("\\\n");
+    } else {
+      state.renderInline(node);
+      state.closeBlock(node);
+    }
+  }
+
+  parseMarkdown() {
+    return { block: "paragraph" };
+  }
+
+  extractLayoutClassFromDOM(dom) {
+    const className = dom.className;
+    const layoutClassMatched = className && className.match(/text-(.*)$/);
+    const layoutClass = layoutClassMatched ? layoutClassMatched[1] : null;
+    return { layoutClass };
+  }
+
+  createDOMAttributes(node) {
+    const className = node.attrs.layoutClass
+      ? `text-${node.attrs.layoutClass}`
+      : null;
+
+    return ["p", { class: className }, 0];
+  }
+
+  isEmptyParagraph(node, state) {
+    return (
+      node.textContent.trim() === "" && node.childCount === 0 && !state.inTable
+    );
+  }
+}
